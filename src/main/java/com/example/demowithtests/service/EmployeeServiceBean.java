264b25c55275a9af8037e1cc224853afdf8f8759
Somov Andrii <som75off@gmail.com>
2023-04-12 22:31:24 +0200

    Updated

--- src/main/java/com/example/demowithtests/service/EmployeeServiceBean.java
+++ src/main/java/com/example/demowithtests/service/EmployeeServiceBean.java
@@ -3,65 +3,68 @@ package com.example.demowithtests.service;
 import com.example.demowithtests.domain.Employee;
 import com.example.demowithtests.repository.Repository;
 import com.example.demowithtests.util.*;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 
-//@AllArgsConstructor
-//@Slf4j
+@AllArgsConstructor
+@Slf4j
+
 @org.springframework.stereotype.Service
 
 public class EmployeeServiceBean implements EmployeeService {
-    private static final Logger log = Logger.getLogger(EmployeeServiceBean.class.getName());
+//    private static final Logger log = Logger.getLogger(EmployeeServiceBean.class.getName());
     private final Repository repository;
 
-    public EmployeeServiceBean(Repository repository) {
-
-        this.repository = repository;
-    }
 
 
     @Override
     public Employee create(Employee employee) {
+        log.debug("Report --> create() - start: employee = {}", employee);
         if (employee.getName().isEmpty()) {
             throw new InvalidEmployeeException();
         }
         Employee createdEmployee;
         createdEmployee = repository.save(employee);
+        log.debug("Report --> create() - is over: employee = {}", createdEmployee);
         return createdEmployee;
     }
 
 
     @Override
     public List<Employee> getAll() {
+        log.debug("Report --> getAll() - start: ");
         List<Employee> employeesList;
         try {
             employeesList = repository.findAll();
         } catch (Exception e) {
             throw new EmployeesNotFoundException();
         }
+        log.debug("Report --> getAll() - is over: ");
         return employeesList;
     }
 
     @Override
     public Employee getById(String id) {
+        log.debug("Report --> getById() - start: id = {}", id);
         Integer empId = Integer.parseInt(id);
         Employee employee = repository.findById(empId).orElseThrow(IdEmployeeNotFoundException::new);
-
         if (employee.getIsDeleted()) {
             throw new DeletedEmployeeException();
         }
-
+        log.debug("Report --> getById() - is over: employee = {}", employee);
         return employee;
     }
 
     @Override
     public Employee updateById(Integer id, Employee employee) {
+        log.debug("Report --> updateById() - start: id = {}, employee = {}", id, employee);
         try {
             return repository.findById(id)
                     .map(entity -> {
@@ -71,6 +74,7 @@ public class EmployeeServiceBean implements EmployeeService {
                         entity.setName(employee.getName());
                         entity.setEmail(employee.getEmail());
                         entity.setCountry(employee.getCountry());
+                        log.debug("Report --> updateById() - is over: employee = {}", entity);
                         return repository.save(entity);
                     })
                     .orElseThrow(IdEmployeeNotFoundException::new);
@@ -81,6 +85,7 @@ public class EmployeeServiceBean implements EmployeeService {
 
     @Override
     public void removeById(Integer id) {
+        log.debug("Report --> removeById() - start: id = {}", id);
         try {
             Employee employee = repository.findById(id)
                     .orElseThrow(IdEmployeeNotFoundException::new);
@@ -88,6 +93,7 @@ public class EmployeeServiceBean implements EmployeeService {
                 throw new DeletedEmployeeException();
             }
             employee.setIsDeleted(true);
+            log.debug("Report --> removeById() - is over: deleted employee = {}", employee);
             repository.save(employee);
         } catch (Exception e) {
             throw new DatabaseAccessException();
@@ -96,39 +102,44 @@ public class EmployeeServiceBean implements EmployeeService {
 
     @Override
     public void removeAll() {
+        log.debug("Report --> removeAll() - start: ");
         try {
             repository.deleteAll();
+            log.debug("Report --> removeAll() - is over: ");
         } catch (DatabaseAccessException e) {
             throw new DeletedEmployeeException();
 
         }
     }
-
     @Override
     public List<Employee> processor() {
-//        log.info("replace null  - start");
+        log.info("replace null  - start");
         List<Employee> replaceNull = repository.findAllByIsDeletedNull();
-//        log.info("replace null after replace: " + replaceNull);
+        log.info("replace null after replace: " + replaceNull);
         for (Employee emp : replaceNull) {
             emp.setIsDeleted(Boolean.FALSE);
         }
-//        log.info("replaceNull = {} ");
-//        log.info("replace null  - end:");
+        log.info("replaceNull = {} ");
+        log.info("replace null  - end:");
         return repository.saveAll(replaceNull);
 
     }
 
     @Override
     public List<Employee> sendEmailByCountry(String country, String text) {
+        log.debug("Report --> sendEmailByCountry() - start: country = {}, text = {}", country, text);
         List<Employee> employees = repository.findAllByCountry(country);
         senderEmails(extracted(employees), text);
+        log.debug("Report --> sendEmailByCountry() - is over: employees = {}", employees);
         return employees;
     }
 
     @Override
     public List<Employee> sendEmailByCity(String city, String text) {
+        log.debug("Report --> sendEmailByCity() - start: city = {}, text = {}", city, text);
         List<Employee> employees = repository.findAllByCity(city);
         senderEmails(extracted(employees), text);
+        log.debug("Report --> sendEmailByCity() - is over: employees = {}", employees);
         return employees;
     }
 
@@ -143,29 +154,35 @@ public class EmployeeServiceBean implements EmployeeService {
 
     @Override
     public void fillDatabase(String quantityString) {
+        log.debug("Report --> fillDatabase() - start:");
         int quantity = Integer.parseInt(quantityString);
         for (int i = 0; i < quantity; i++) {
             repository.save(createEmployee("name", "country", "email"));
         }
+        log.debug("Report --> fillingDataBase() is over: " + quantityString + " employees");
     }
 
     @Override
     public void updateByCountry(String country) {
+        log.debug("Report --> updateByCountry() - start:");
         List<Employee> employees = repository.findAll();
         for (Employee employee : employees) {
             employee.setCountry(country);
             repository.save(employee);
         }
+        log.debug("Report --> updateByCountry() - is over:");
     }
     @Override
     @Transactional
     public void cleverUpdateByCountry(String oldCountry, String newCountry) {
+        log.debug("Report --> cleverUpdateByCountry() - start:");
         List<Employee> employees = repository.findAll();
         for (Employee employee : employees)
             if (employee.getCountry().equals(oldCountry)) {
                 employee.setCountry(newCountry);
                 repository.save(employee);
             }
+        log.debug("Report --> cleverUpdateByCountry() - is over:");
     }
     public String randomCountry() {
         List<String> countries = countryGenerator();
@@ -178,10 +195,12 @@ public class EmployeeServiceBean implements EmployeeService {
 
     @Override
     public Employee createEmployee(String name, String country, String email) {
+        log.debug("Report --> createEmployee() - is over:");
         return new Employee(name, country, email);
     }
     @Override
     public void sendEmailAllWhoMovedFrom(String country){
+        log.debug("Report --> sendEmailAllWhoMovedFrom() - start: country = {}", country);
         List<Employee> employeeListFrom = repository.findAllByCountry(country);
         List<Employee> employeesListMovedFrom = employeeListFrom.stream().filter(employee -> employee.getAddresses().stream()
                         .anyMatch(address -> address.getCountry().equals(country)
@@ -189,13 +208,13 @@ public class EmployeeServiceBean implements EmployeeService {
                 .collect(Collectors.toList());
         String text = "Putin died like a mad dog! Go back home!";
         senderEmails(extracted(employeesListMovedFrom),text);
+        log.debug("Report --> sendEmailByCity() - is over: employeeListFrom = {}", employeeListFrom );
         System.out.println("Sent emails " + employeesListMovedFrom.size());
 
     }
 
     public void senderEmails(List<String> emails, String text) {
-
-//        log.info("Sending emails to: " + emails);
+        log.info("Sending emails to: " + emails);
     }
 
 
--- src/main/java/com/example/demowithtests/web/EmployeeControllerBean.java
+++ src/main/java/com/example/demowithtests/web/EmployeeControllerBean.java
@@ -9,13 +9,16 @@ import com.example.demowithtests.util.EmployeesNotFoundException;
 import com.example.demowithtests.util.config.Mapper;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
+@Slf4j
 @RestController
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 @Tag(name = "Employee", description = "Employee API")
@@ -26,12 +29,15 @@ public class EmployeeControllerBean implements EmployeeController {
 
     @Override
     public EmployeeDto saveEmployee(@RequestBody EmployeeDto employeeDto) {
+        log.info("Controller --> saveEmployee() - start: ");
         Employee employee = mapper.employeeDtoToEmployee(employeeDto);
+        log.info("Controller --> saveEmployee() is over, employee " + employee + " created");
         return mapper.employeeToEmployeeDto(employeeService.create(employee));
     }
 
     @Override
     public List<EmployeeReadDto> getAllUsers() {
+        log.info("Controller --> getAllUsers() - start: ");
         List<Employee> employees = employeeService.getAll();
         List<EmployeeReadDto> employeesReadDto = new ArrayList<>();
         for (Employee employee : employees) {
@@ -39,44 +45,59 @@ public class EmployeeControllerBean implements EmployeeController {
                     mapper.employeeToEmployeeReadDto(employee)
             );
         }
+        log.info("Controller --> getAllUsers() - is over");
         return employeesReadDto;
     }
 
     @Override
     public EmployeeReadDto getEmployeeById(@PathVariable String id) {
-        return mapper.employeeToEmployeeReadDto(
-                employeeService.getById(id)
-        );
+        log.info("Controller --> getEmployeeById() - start: id={}", id);
+        Employee employee = employeeService.getById(id);
+        if (employee == null) {
+            log.error("Controller --> getEmployeeById() - employee with id {} not found", id);
+            throw new EntityNotFoundException("Employee not found with id " + id);
+        }
+        log.info("Controller --> getEmployeeById() - is over: id={}", id);
+        return mapper.employeeToEmployeeReadDto(employee);
     }
 
     @Override
     public EmployeeReadDto refreshEmployee(@PathVariable("id") String id, @RequestBody EmployeeDto employeeDto) throws EmployeesNotFoundException {
+        log.info("Controller --> refreshEmployee() - start: ");
         Integer parseId = Integer.parseInt(id);
+        log.info("Controller --> refreshEmployee() - is over: id={}", id);
         return mapper.employeeToEmployeeReadDto(
                 employeeService.updateById(parseId, mapper.employeeDtoToEmployee(employeeDto)
                 )
         );
     }
-
     @Override
     public void removeEmployeeById(@PathVariable String id) {
+        log.info("Controller --> removeEmployeeById() - start: ");
         Integer parseId = Integer.parseInt(id);
         employeeService.removeById(parseId);
+        log.info("Controller --> removeEmployeeById() - is over: id={}", id);
     }
 
     @Override
     public void removeAllUsers() {
+        log.info("Controller --> removeAllUsers() - start: ");
         employeeService.removeAll();
+        log.info("Controller --> removeAllUsers() - is over");
     }
 
     @Override
     public void sendEmailByCountry(@RequestParam String country, @RequestParam String text) {
+        log.info("Controller --> sendEmailByCountry() - start: country={}, text={}", country, text);
         employeeService.sendEmailByCountry(country, text);
+        log.info("Controller --> sendEmailByCountry() - is over");
     }
 
     @Override
     public void sendEmailByCity(@RequestParam String cities, @RequestBody String text) {
+        log.info("Controller --> sendEmailByCity() - start: cities={}, text={}", cities, text);
         employeeService.sendEmailByCity(cities, text);
+        log.info("Controller --> sendEmailByCity() - is over");
     }
 
     @Override
@@ -86,27 +107,37 @@ public class EmployeeControllerBean implements EmployeeController {
 
     @Override
     public void fillDatabase(@PathVariable String quantity) {
+        log.info("Controller --> fillDatabase() - start: quantity={}", quantity);
         employeeService.fillDatabase(quantity);
+        log.info("Controller --> fillDatabase() - is over");
     }
 
     @Override
     public void updateByCountry(@RequestParam String countries) {
+        log.info("Controller --> updateByCountry() - start: countries={}", countries);
         employeeService.updateByCountry(countries);
+        log.info("Controller --> updateByCountry() - is over");
     }
 
     @Override
     public void cleverUpdateByCountry(@RequestParam String oldCountry, String newCountry) {
+        log.info("Controller --> cleverUpdateByCountry() - start: oldCountry={}, newCountry={}", oldCountry, newCountry);
         employeeService.cleverUpdateByCountry(oldCountry, newCountry);
+        log.info("Controller --> cleverUpdateByCountry() - is over");
     }
 
     @Override
     public void sendEmailAllWhoMovedFrom(@RequestParam String country) {
+        log.info("Controller --> sendEmailAllWhoMovedFrom() - start: country={}", country);
         employeeService.sendEmailAllWhoMovedFrom(country);
+        log.info("Controller --> sendEmailAllWhoMovedFrom() - is over");
     }
 
     @Override
     public void replaceNull() {
+        log.info("Controller --> replaceNull() - start: ");
         employeeService.processor();
+        log.info("Controller --> replaceNull() - is over");
     }
 
 
