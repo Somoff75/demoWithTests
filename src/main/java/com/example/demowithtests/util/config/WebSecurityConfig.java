408491d1d0ebbdc1d7a3be67d41785a489948ffd
Somov Andrii <som75off@gmail.com>
2023-05-30 09:50:48 +0200

    Updated

---
+++ src/main/java/com/example/demowithtests/util/config/WebSecurityConfig.java
@@ -0,0 +1,66 @@
+package com.example.demowithtests.util.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Autowired
+//    private DataSource;
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeHttpRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/**").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PATCH, "/api/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().disable()
+                .csrf().disable();
+    }
+
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+
+                .withUser("user").password("{noop}password").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
+
+    }
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.jdbcAuthentication()
+//                .dataSource(dataSource)
+//                .usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username = ?")
+//                .authoritiesByUsernameQuery("SELECT username, role FROM user_roles WHERE username = ?")
+//                .passwordEncoder(passwordEncoder());
+//    }
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
+}
--- src/main/resources/db/migration/V5__add_table_top.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Top (
-                     id SERIAL PRIMARY KEY,
-                     country VARCHAR(255),
-                     email VARCHAR(255),
-                     is_deleted BOOLEAN,
-                     name VARCHAR(255),
-                     passport_id INTEGER,
-                     gender VARCHAR
-);
